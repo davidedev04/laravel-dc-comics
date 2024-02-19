50aa1a813bd9c5039cb8bdd415a0633476814d4e
Davide Prencipe <davidedev04@gmail.com>
2024-02-19 18:27:41 +0100

    finito esercizio di giovedi 15/02

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::create('comics', function (Blueprint $table) {
+            $table -> id();
+
+            $table -> string('title');
+            $table -> text('description') -> nullable();
+            $table -> decimal('price');
+
+            $table -> timestamps();
         });
     }
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comics');
     }
 };
--- resources/views/pages/comic/index.blade.php
+++ resources/views/pages/comic/index.blade.php
@@ -12,14 +12,14 @@
         <a href="{{ route('comic.show', $comic -> id) }}">
             {{ $comic -> title }}
         </a>
-        <a class="ms-3" href="{{ route('comic.edit', $comic -> id) }}">Edit</a>
+        <!-- <a class="ms-3" href="{{ route('comic.edit', $comic -> id) }}">Edit</a>
         <form class="ms-3 d-inline-block" action="{{ route('comic.destroy', $comic -> id) }}" method="POST"
             onsubmit="return confirm('Confermare?');">
 
             @csrf
             @method('DELETE')
 
-            <input type="submit" value="DELETE">
+            <input type="submit" value="DELETE"> -->
         </form>
     </li>
     @endforeach
